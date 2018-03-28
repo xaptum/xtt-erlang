746a77e718b8df0ff81e3865c16a767b33b602e0
Irina Guberman <irina.guberman@gmail.com>
2018-03-28 11:42:39 -0500

    possible fix of vars on the stack problems

--- c_src/xtt_erlang.c
+++ c_src/xtt_erlang.c
@@ -339,30 +339,7 @@ xtt_start_client_handshake(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     ErlNifBinary temp_bin;
     ERL_NIF_TERM ret_code = enif_make_int(env, rc);
 
-//    switch(rc){
-//            case XTT_RETURN_WANT_READ:
-//                puts("Building response for XTT_RETURN_WANT_READ\n");
-//                return enif_make_tuple3(env, ret_code, enif_make_int(env, cs->bytes_requested), cs_term);
-//            case XTT_RETURN_WANT_WRITE:
-//                puts("Building response for XTT_RETURN_WANT_WRITE\n");
-//                printf("Creating write buffer of length %d from %p\n", cs->bytes_requested, cs->io_ptr);
-//                enif_alloc_binary(cs->bytes_requested, &temp_bin);
-//                memcpy(temp_bin.data, cs->io_ptr, cs->bytes_requested);
-//
-//                return enif_make_tuple3(env, ret_code, enif_make_binary(env, &temp_bin), cs_term);
-//            case XTT_RETURN_WANT_BUILDIDCLIENTATTEST:
-//                puts("Building response for XTT_RETURN_WANT_BUILDIDCLIENTATTEST\n");
-//
-//                //typedef struct {unsigned char data[16];} xtt_certificate_root_id;
-//                enif_alloc_binary(sizeof(xtt_certificate_root_id), &temp_bin);
-//                memcpy(temp_bin.data, &(cs->claimed_root_id), sizeof(xtt_certificate_root_id));
-//                return enif_make_tuple3(env, ret_code, enif_make_binary(env, &temp_bin), cs_term);
-//            default:
-//                printf("Building default response for %d\n", rc);
-//                return enif_make_tuple2(env, ret_code, cs_term);
-//    }
-
-    return build_response(env, rc, ret_code, &cs_term, cs, &temp_bin);
+    return build_response(env, rc, &cs_term, cs, &temp_bin);
 }
 
 static ERL_NIF_TERM
@@ -407,9 +384,9 @@ xtt_client_handshake(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
                                &(cs->io_ptr),
                                &(cs->ctx));
 
-        ErlNifBinary *temp_bin;
+    ErlNifBinary *temp_bin;
 
-        return build_response(env, rc, &cs_term, cs, &temp_bin);
+    return build_response(env, rc, &cs_term, cs, &temp_bin);
 }
 
 static ERL_NIF_TERM
