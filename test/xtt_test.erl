21861e3f0c53d0e7a22ff80c416b952c04856076
Irina Guberman <irina.guberman@xaptum.com>
2018-04-27 18:29:17 -0500

    Use the same Tcti throughout

--- test/xtt_test.erl
+++ test/xtt_test.erl
@@ -153,7 +153,7 @@ group_context_inputs(#{
           Gid = crypto:hash(sha256, Gpk),
 
           PrivKeyInputs = #priv_key_tpm{key_handle = ?KEY_HANDLE,
-            tcti_context = undefined,   %% TODO: passing existing Tcti context ref to a different process appears to kill TPM simulator
+            tcti_context = TctiContext,
             tpm_host = TpmHostname, tpm_port = TpmPort, tpm_password = TpmPassword},
 
           ok = initialize_certsTPM(SapiContext),
